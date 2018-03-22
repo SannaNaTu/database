52433a3361de09b9c693cc85baa184a2b6596475
SannaNaTu <korhonsm@gmail.com>
2018-03-22 10:20:57 +0200

    Update

---
+++ Tehtävä 1/Tehtävä 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "HEllo World");
+        }
+    }
+}
